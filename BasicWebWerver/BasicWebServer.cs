23749e4b14260958f30b4d005b4a0a068bb3f01c
Susan Dwyer <suzycream999@googlemail.com>
2024-03-19 07:07:42 +0000

    Add project files.

---
+++ BasicWebWerver/BasicWebServer.cs
@@ -0,0 +1,165 @@
+﻿using System.Net;
+using System.Runtime.CompilerServices;
+
+class BasicWebServer
+{
+    private HttpListener listener;
+    private string directoryPath;
+    private bool isRunning = false;
+
+    public BasicWebServer(string directoryPath)
+    {
+        this.directoryPath = directoryPath;
+        listener = new HttpListener();
+    }
+
+    public async Task StartServer(int port)
+    {
+        if (!HttpListener.IsSupported)
+        {
+            Msg("HttpListener not supported.", MsgType.Error);
+            return;
+        }
+
+        if (!Directory.Exists(directoryPath))
+        {
+            Msg("Root directory does not exist.", MsgType.Error);
+            return;
+        }
+
+        listener.Prefixes.Add($"http://localhost:{port}/");
+        listener.Start();
+        Msg($"Server started. Listening on port {port}...", MsgType.Info);
+        isRunning = true;
+
+        while (listener.IsListening)
+        {
+            try
+            {
+                HttpListenerContext context = await listener.GetContextAsync();
+                if (context != null)
+                {
+                    ProcessRequest(context);
+                }
+            }
+            catch (Exception ex)
+            {
+                Msg(ex.Message, MsgType.Error);
+            }
+        }
+    }
+
+    private void ProcessRequest(HttpListenerContext context)
+    {
+        string? filename = context.Request.RawUrl?.TrimStart('/');
+
+        if(!context.Request.IsLocal)
+        {
+            Msg("Non local request - ignored", MsgType.Alert);
+            context.Response.Close();
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(filename))
+        {
+            filename = "index.html";
+        }
+
+        Msg($"{context.Request.HttpMethod}: {context.Request.Url}", MsgType.Request);
+
+        string filePath = Path.Combine(directoryPath, filename);
+
+
+        if (File.Exists(filePath))
+        {
+            try
+            {
+                context.Response.ContentType = "text/html";
+                Stream pageContent = new FileStream(filePath, FileMode.Open);
+                pageContent.CopyTo(context.Response.OutputStream);
+                context.Response.Close();
+                pageContent.Close();
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                if (msg.Contains("specified network"))
+                {
+                    msg += " : " + context.Request.QueryString;
+                }
+                Msg(msg, MsgType.Error);
+            }
+        }
+        else
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+            context.Response.Close();
+        }
+    }
+
+    public bool IsRunning()
+    {
+        return isRunning;
+    }
+
+    public void StopServer()
+    {
+        try
+        {
+            if (listener != null && listener.IsListening)
+            {
+                listener.Stop();
+                listener.Close();
+                Msg("Server stopped.", MsgType.Info);
+                isRunning = false;
+            }
+        }
+        catch (Exception ex)
+        {
+            Msg(ex.Message, MsgType.Error);
+            isRunning = true;
+        }
+    }
+
+    private void Msg(string msg, MsgType type, [CallerMemberName] string membername = "")
+    {
+        if (type == MsgType.Error)
+        {
+            if (msg.Contains("Thread", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+            if (isRunning)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Server error:");
+            }
+        }
+        else if (type == MsgType.Request)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Client request:");
+        }
+        else if (type == MsgType.Alert)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Server alert:");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Server msg:");
+        }
+        Console.WriteLine("\t" + membername);
+        Console.WriteLine("\t" + msg);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
+enum MsgType
+{
+    Error,
+    Request,
+    Info,
+    Alert
+}
---
+++ BasicWebWerver/Program.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static string directoryPath = @"C:\Users\creamwx\Music\";
+    
+    private static BasicWebServer? server;// = new SimpleHttpServer(directoryPath);
+    public static bool IsRunning { get; private set; }
+    private static string NewLine = Environment.NewLine;
+    public static string StartPrompt { get; private set; } = 
+            $" Space Bar: Start server using defasults.{NewLine}" +
+            $" d: Set root directory to serve.{NewLine}" +
+            $" p: Set port.{NewLine}" +
+            $" Esc: Exit";
+
+    
+    private static int port = 8000;
+
+    static Task Main(string[] args)
+    {
+        Prompt(StartPrompt);
+        while (true)
+        {
+            var StartKey = Console.ReadKey(true);
+            if (StartKey.Key == ConsoleKey.Spacebar)
+            {
+                StartServer(directoryPath, port);
+                break;
+            }
+            else if (StartKey.Key == ConsoleKey.D)
+            {
+                SetRoot();
+                continue;
+            }
+            else if (StartKey.Key == ConsoleKey.P)
+            {
+                Setport();
+                continue;
+            }
+            else if (StartKey.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
+            }
+            //
+
+        }
+        //Environment.Exit(0);
+        //int port = 8000;
+        //StartServer(port);
+
+        while (true)
+        {
+            var key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Escape)
+            {
+                StopServer(server);
+
+                Prompt("Space to restart. Esc to exit");
+                while (true)
+                {
+                    key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.Escape)
+                    {
+                        Environment.Exit(0);
+                    }
+                    else if (key.Key == ConsoleKey.Spacebar)
+                    {
+                        Console.Clear();
+                        StartServer(directoryPath, port);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private static void Setport()
+    {
+        Console.WriteLine("Enter port number.");
+        try
+        {
+            port = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"Port set: {port}");
+            Prompt(StartPrompt);
+        }
+        catch (Exception)
+        {
+            Console.Clear();
+            Console.WriteLine("Error parsing port string. Try again.");
+            Prompt(StartPrompt);
+        }
+    }
+
+    private static void SetRoot()
+    {
+        Console.WriteLine("Enter path to root.");
+        string dir = Console.ReadLine();
+        if(Directory.Exists(dir))
+        {
+            directoryPath = dir;
+            Console.Clear() ;
+            Console.WriteLine($"Root set: {directoryPath}");
+            Prompt(StartPrompt);
+            return;
+        }
+        Console.Clear ();
+        Console.WriteLine("Directory does not exist: Wrap in quotes and try again.");
+        Prompt(StartPrompt);
+    }
+
+    private static void Prompt(string msg)
+    {
+        Console.WriteLine();
+        Console.WriteLine("########### Basic HTTP Web Server ##########");
+        //Console.Write("\t");
+        Console.WriteLine(msg);
+        Console.WriteLine("############################################");
+        Console.WriteLine();
+    }
+
+    private static void StartServer(string dir, int port)
+    {
+        directoryPath = dir;
+        server = new BasicWebServer(directoryPath);
+        _ = server.StartServer(port);
+        IsRunning = true;
+        SetConsoleTitle(directoryPath, port);
+    }
+
+    private static void SetConsoleTitle(string directoryPath = "", int port = 0)
+    {
+        if (!IsRunning)
+        {
+            Console.Title = "Stopped";
+            return;
+        }
+        Console.Title = $"Serving: {directoryPath} on port {port}";
+    }
+
+    static string? GenerateHtml(List<string> fileList, string indexFilePath)
+    {
+        string indexStringPath = indexFilePath.Replace(".html", ".txt");
+        string html = File.ReadAllText(indexStringPath);
+
+        // Insert items intp list
+        var splitHtml = html.Split("<!--list item-->");
+        var htmlTop = splitHtml[0];
+        var htmlBottom = splitHtml[1];
+
+        foreach (var fileName in fileList)
+        {
+            htmlTop += WrapInListTags(fileName);
+        }
+
+        return htmlTop + htmlBottom;
+    }
+
+    static void Refresh()
+    {
+        //Console.WriteLine("refreshind folder...");
+        var filePathList = Directory.GetFiles(directoryPath, "*.mp4").ToList();
+        List<string> fileNameList = new List<string>();
+        foreach (string filePath in filePathList)
+        {
+            fileNameList.Add(Path.GetFileName(filePath));
+        }
+        string indexFilePath = Path.Combine(directoryPath, "index.html");
+        string indexHtml = GenerateHtml(fileNameList, indexFilePath);
+
+        File.WriteAllText(indexFilePath, indexHtml);
+    }
+
+    static string WrapInListTags(string fileName)
+    {
+        return $"""<li><a href="{fileName}">{fileName}</a></li>{Environment.NewLine}""";
+    }
+
+    private static void StopServer(BasicWebServer server)
+    {
+        server.StopServer();
+        IsRunning = false;
+        SetConsoleTitle();
+    }
+}
+
